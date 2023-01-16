58e4a621ea522b014eb754c64fc37c4c56019722
lzt <liuzetan1988@126.com>
2023-01-16 15:03:57 +0800

    : 视频加载失败后，避免动画重复执行，如果重复执行动画，会导致后续的 InteractionManager.runAfterInteractions 无法执行
    --bug=1035162 --user=刘泽潭 【灰度】【app】【员工多选】员工多选字段, 按部门选同事, 手动选择几个人后, 确定, 回到了上页, 但未带入所选员工, 再次点击确定, 未带出员工(仅android如此, iOS是选完员工后直接回到了新建页面, 未回到上页) https://www.tapd.cn/23787791/s/1631584

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -89,7 +89,7 @@ export default class VideoPlayer extends Component {
      * Our app listeners and associated methods
      */
     this.events = {
-      onError: this.props.onError || this._onError.bind(this),
+      onError: this._onError.bind(this),
       onBack: this.props.onBack || this._onBack.bind(this),
       onEnd: this.props.onEnd || this._onEnd.bind(this),
       onScreenTouch: this._onScreenTouch.bind(this),
@@ -291,6 +291,9 @@ export default class VideoPlayer extends Component {
     state.loading = false;
 
     this.setState(state);
+    if (typeof this.props.onError === 'function') {
+      this.props.onError(err);
+    }
   }
 
   /**
